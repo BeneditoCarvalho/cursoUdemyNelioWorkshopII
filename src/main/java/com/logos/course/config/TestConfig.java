89ea0446195d05f6720fe1a7a56a4dec78050dca
Benedito Carvalho <benedito.carvalho@gmail.com>
2021-08-24 22:30:10 -0300

    Category

--- src/main/java/com/logos/course/config/TestConfig.java
+++ src/main/java/com/logos/course/config/TestConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.logos.course.entities.Category;
 import com.logos.course.entities.Order;
 import com.logos.course.entities.User;
 import com.logos.course.entities.enums.OrderStatus;
+import com.logos.course.repositories.CategoryRepository;
 import com.logos.course.repositories.OrderRepository;
 import com.logos.course.repositories.UserRepository;
 
@@ -23,9 +25,16 @@ public class TestConfig implements CommandLineRunner {
 
 	@Autowired
 	private OrderRepository orderRepository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
+		
+		Category cat1 = new Category(null, "Electronics");
+		Category cat2 = new Category(null, "Books");
+		Category cat3 = new Category(null, "Computers");
 
 		User u = new User(null, "Bene", "bene@gmail.com", "333333", "4545454");
 		User u2 = new User(null, "Jão", "jao@gmail.com", "555555", "23232323");
@@ -33,6 +42,8 @@ public class TestConfig implements CommandLineRunner {
 		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u);
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT,u2);
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT,u);
+		
+		categoryRepository.saveAll(Arrays.asList(cat1,cat2, cat3));
 
 		userRepository.saveAll(Arrays.asList(u, u2));
 		
---
+++ src/main/java/com/logos/course/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.logos.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.logos.course.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
