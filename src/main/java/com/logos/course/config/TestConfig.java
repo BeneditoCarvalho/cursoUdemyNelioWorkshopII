b50e8c5bcf1a1fe0acad7a20a51ea5dfe98a4171
Benedito Carvalho <benedito.carvalho@gmail.com>
2021-08-24 21:28:45 -0300

    OrderStatus Enum

--- src/main/java/com/logos/course/config/TestConfig.java
+++ src/main/java/com/logos/course/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.logos.course.entities.Order;
 import com.logos.course.entities.User;
+import com.logos.course.entities.enums.OrderStatus;
 import com.logos.course.repositories.OrderRepository;
 import com.logos.course.repositories.UserRepository;
 
@@ -29,9 +30,9 @@ public class TestConfig implements CommandLineRunner {
 		User u = new User(null, "Bene", "bene@gmail.com", "333333", "4545454");
 		User u2 = new User(null, "Jão", "jao@gmail.com", "555555", "23232323");
 
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u);
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u);
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT,u2);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT,u);
 
 		userRepository.saveAll(Arrays.asList(u, u2));
 		
