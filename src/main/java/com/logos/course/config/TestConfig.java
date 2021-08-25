bdc229aaa9ea00a4d5a9388ce79173754b875fc6
Benedito Carvalho <benedito.carvalho@gmail.com>
2021-08-25 10:08:29 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/logos/course/config/TestConfig.java
+++ src/main/java/com/logos/course/config/TestConfig.java
@@ -47,16 +47,24 @@ public class TestConfig implements CommandLineRunner {
 		Category cat2 = new Category(null, "Books");
 		Category cat3 = new Category(null, "Computers");
 
+		productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));		
+		categoryRepository.saveAll(Arrays.asList(cat1,cat2, cat3));
+		
+		p1.getCategories().add(cat2);
+		p2.getCategories().add(cat1);
+		p2.getCategories().add(cat3);
+		p3.getCategories().add(cat3);
+		p4.getCategories().add(cat3);
+		p5.getCategories().add(cat2);
+		
+		productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
+		
 		User u = new User(null, "Bene", "bene@gmail.com", "333333", "4545454");
 		User u2 = new User(null, "Jão", "jao@gmail.com", "555555", "23232323");
 
 		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u);
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT,u2);
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT,u);
-		
-		productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
-		
-		categoryRepository.saveAll(Arrays.asList(cat1,cat2, cat3));
 
 		userRepository.saveAll(Arrays.asList(u, u2));
 		
